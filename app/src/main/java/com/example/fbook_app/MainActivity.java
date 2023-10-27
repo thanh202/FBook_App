77cbd09a1a56a7b0d2328d69d3f99a9f0fb1d114
giangpham <giangpdph27260@fpt.edu.vn>
2023-10-27 23:53:44 +0700

    Front_end màn hình Đăng nhập, Đăng ký, Đăng nhập home

--- app/src/main/java/com/example/fbook_app/MainActivity.java
+++ app/src/main/java/com/example/fbook_app/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.fbook_app;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.fbook_app.dangNhap.DangKyActivity;
+import com.example.fbook_app.dangNhap.DangNhapActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView btnLoginWithGoogle;
+    private TextView btnToSignUp;
+    private TextView btnToLogin;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLoginWithGoogle = (TextView) findViewById(R.id.btn_login_with_google);
+        btnToSignUp = (TextView) findViewById(R.id.btn_to_sign_up_home);
+        btnToLogin = (TextView) findViewById(R.id.btn_to_login_home);
+
+        btnToLogin.setOnClickListener(v -> {
+            startActivity(new Intent(this, DangNhapActivity.class));
+        });
+        btnToSignUp.setOnClickListener(v -> {
+            startActivity(new Intent(this, DangKyActivity.class));
+        });
     }
 }
---
+++ app/src/main/java/com/example/fbook_app/dangNhap/DangKyActivity.java
@@ -0,0 +1,24 @@
+package com.example.fbook_app.dangNhap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fbook_app.MainActivity;
+import com.example.fbook_app.R;
+
+public class DangKyActivity extends AppCompatActivity {
+    private ImageView btnBack;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dangky);
+        btnBack = findViewById(R.id.rl_btn_back_signUp);
+        btnBack.setOnClickListener(v -> {
+            startActivity(new Intent(this, MainActivity.class));
+        });
+    }
+}
