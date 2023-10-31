3b29d3060e38312e2fa58d06047485e8e8f39999
Linh203 <115698525+Linh203@users.noreply.github.com>
2023-10-31 21:08:43 +0700

    Home

--- app/src/main/java/com/example/fbook_app/dangNhap/DangNhapActivity.java
+++ app/src/main/java/com/example/fbook_app/dangNhap/DangNhapActivity.java
@@ -1,7 +1,9 @@
 package com.example.fbook_app.dangNhap;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -9,24 +11,36 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.fbook_app.Doi_Mat_khau.ForgotPassword_Activity;
+import com.example.fbook_app.HomeActivity.HomeActivity;
 import com.example.fbook_app.MainActivity;
 import com.example.fbook_app.R;
 
 public class DangNhapActivity extends AppCompatActivity {
     private ImageView btnBack;
-    private TextView btnForgotPass;
+    private TextView btnForgotPass, btnLoginHome;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dangnhap);
         btnBack = findViewById(R.id.rl_btn_back_login);
         btnForgotPass = findViewById(R.id.btn_forgotPassword_login);
+        btnLoginHome = findViewById(R.id.btn_login_home);
 
         btnBack.setOnClickListener(v -> {
             startActivity(new Intent(this, MainActivity.class));
         });
         btnForgotPass.setOnClickListener(v -> {
             startActivity(new Intent(this, ForgotPassword_Activity.class));
+        });
+        btnLoginHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(DangNhapActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finishAffinity();
+            }
         });
     }
 }
