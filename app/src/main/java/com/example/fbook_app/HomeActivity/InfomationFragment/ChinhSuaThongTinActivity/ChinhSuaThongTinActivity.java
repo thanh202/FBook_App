b3e35e6997fe53efcaaaf67c059328ed3a887e80
Linh203 <115698525+Linh203@users.noreply.github.com>
2023-11-04 19:19:45 +0700

    Font-End (Chỉnh Sửa Thông Tin)

---
+++ app/src/main/java/com/example/fbook_app/HomeActivity/InfomationFragment/ChinhSuaThongTinActivity/ChinhSuaThongTinActivity.java
@@ -0,0 +1,16 @@
+package com.example.fbook_app.HomeActivity.InfomationFragment.ChinhSuaThongTinActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fbook_app.R;
+
+public class ChinhSuaThongTinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chinh_sua_thong_tin);
+    }
+}
--- app/src/main/java/com/example/fbook_app/HomeActivity/InfomationFragment/InfomationFragment.java
+++ app/src/main/java/com/example/fbook_app/HomeActivity/InfomationFragment/InfomationFragment.java
@@ -1,5 +1,6 @@
 package com.example.fbook_app.HomeActivity.InfomationFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +8,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.example.fbook_app.HomeActivity.InfomationFragment.ChinhSuaThongTinActivity.ChinhSuaThongTinActivity;
 import com.example.fbook_app.R;
 
 
 public class InfomationFragment extends Fragment {
 
 
-
+private LinearLayout btnChinhSuaThongTin;
+private View view;
     public InfomationFragment() {
         // Required empty public constructor
     }
@@ -31,6 +35,18 @@ public class InfomationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_infomation, container, false);
+        view= inflater.inflate(R.layout.fragment_infomation, container, false);
+
+        btnChinhSuaThongTin=view.findViewById(R.id.btn_chinhsuathongtin);
+
+        btnChinhSuaThongTin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getContext(), ChinhSuaThongTinActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
