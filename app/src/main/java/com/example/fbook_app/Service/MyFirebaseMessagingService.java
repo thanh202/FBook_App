466435190d7b242cfb81b76949799e51e75ace0e
Linh203 <115698525+Linh203@users.noreply.github.com>
2024-01-03 08:27:23 +0700

    Push Notification (FireBase)

--- app/src/main/java/com/example/fbook_app/Service/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/fbook_app/Service/MyFirebaseMessagingService.java
@@ -4,6 +4,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.widget.RemoteViews;
@@ -11,7 +12,9 @@ import android.widget.RemoteViews;
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 
+import com.example.fbook_app.HomeActivity.HomeActivity;
 import com.example.fbook_app.MainActivity;
+import com.example.fbook_app.MyApplication;
 import com.example.fbook_app.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -20,36 +23,29 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(@NonNull RemoteMessage message) {
         super.onMessageReceived(message);
-        if (message.getNotification() != null) {
-            showNotification(message.getNotification().getTitle(), message.getNotification().getBody());
-        }
+
+        RemoteMessage.Notification notification = message.getNotification();
+        if (notification == null)
+            return;
+
+        String title = notification.getTitle();
+        String body = notification.getBody();
+
+        sendNotification(title, body);
     }
 
-    private void showNotification(String title, String body) {
-        String channelId = "nofi";
+    private void sendNotification(String title, String body) {
         Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setAutoCancel(true)
-                .setVibrate(new long[]{1000, 1000, 1000, 1000})
-                .setOnlyAlertOnce(true)
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, MyApplication.ID)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(R.drawable.logo_fbook)
                 .setContentIntent(pendingIntent);
-        builder = builder.setContent(customView(title, body));
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel notificationChannel = new NotificationChannel(channelId, "web_app", NotificationManager.IMPORTANCE_HIGH);
-            notificationManager.createNotificationChannel(notificationChannel);
+        Notification notification = builder.build();
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (manager != null) {
+            manager.notify(1, notification);
         }
-        notificationManager.notify(0, builder.build());
     }
-
-    private RemoteViews customView(String title, String body) {
-        RemoteViews remoteViews = new RemoteViews(getApplication().getPackageName(), R.layout.notifycation_layout);
-        remoteViews.setTextViewText(R.id.notify_title, title);
-        remoteViews.setTextViewText(R.id.notify_body, body);
-        return remoteViews;
-    }
-
 }
